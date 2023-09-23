263e4b84d2b536468616f5f0651e327f3259cdec
Stanzin7 <Stanzin.norzang@wartburg.edu>
2023-09-23 18:24:17 -0500

    Add models for tours and users; Update tour controller and routes; Include .gitignore and modify configuration

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const tourController = require('./../controllers/tourController');
+const express = require("express");
+const tourController = require("./../controllers/tourController");
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
-
 router
-  .route('/')
+  .route("/")
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(tourController.getTour)
   .patch(tourController.updateTour)
   .delete(tourController.deleteTour);
--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-const dotenv = require('dotenv');
-const app = require('./app');
+const dotenv = require("dotenv");
+const app = require("./app");
+const mongoose = require("mongoose");
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+// Connect to MongoDB after dotenv config
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    console.log("DB connection failed");
+    console.log(err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
